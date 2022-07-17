20e09ca3ea9a3e08d1085b44fb7d8cafee8b7c7f
Alexander <alexander.c1989@gmail.com>
2022-07-17 01:52:02 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react'
 import Home from './Home'
 import Error404 from './Error404'
-import SignUpForm from './users/SignUpForm'
-import LoginForm from './users/LoginForm'
+import SignUpForm from './users/SignUpForm.js'
+import LoginForm from './users/LoginForm.js'
+import CurrentUserProvider from './contexts/CurrentUser.js'
+import Navigation from './Navigation'
 
 function App() {
   return (
     <CurrentUserProvider>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" component={Home} />
-        <Route path="/sign-up" component={SignUpForm} />
-        <Route path="/login" component={LoginForm} />
-        {/* <Route path="/" component={Error404} /> */}
-      </Routes>
+    <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sign-up" component={SignUpForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route path="/" component={Error404} />
+      </Switch>
       </BrowserRouter>
     </CurrentUserProvider>
     
--- src/users/SignupForm.js
+++ src/users/SignupForm.js
@@ -3,74 +3,72 @@ import { useHistory, useParams } from "react-router"
 
 function SignUpForm() {
 
-	const history = useHistory()
+    const history = useHistory()
 
-	const [user, setUser] = useState({
-		userName: '',
-		password: '',
+    const [user, setUser] = useState({
+        userName: '',
+        password: '',
         displayName: ''
-	})
+    })
 
-	async function handleSubmit(e) {
-		e.preventDefault()
+    async function handleSubmit(e) {
+        e.preventDefault()
 
-		await fetch(`http://localhost:3000/users/`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(user)
-		})
+        await fetch(`http://localhost:3000/users/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
 
-		history.push(`/`)
-	}
+        history.push(`/`)
+    }
 
-	return (
-		<main>
-			<h1>Sign Up</h1>
-			<form onSubmit={handleSubmit}>
-				<div className="row">
-					<div className="col-sm-6 form-group">
-						<label htmlFor="userName">User Name</label>
-						<input
-							required
-							value={user.userName}
-							onChange={e => setUser({ ...user, userName: e.target.value })}
-							className="form-control"
-							id="userName"
-							name="userName"
-						/>
-					</div>
-				</div>
-                <div className="col-sm-6 form-group">
-						<label htmlFor="displayName">Display Name</label>
-						<input
-							required
-							value={user.displayName}
-							onChange={e => setUser({ ...user, displayName: e.target.value })}
-							className="form-control"
-							id="displayName"
-							name="displayName"
-						/>
-					</div>
-				<div className="row">
-					<div className="col-sm-6 form-group">
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col-sm-6 form-group">
+                        <label htmlFor="userName">User Name</label>
+                        <input
+                            required
+                            value={user.userName}
+                            onChange={e => setUser({ ...user, userName: e.target.value })}
+                            className="form-control"
+                            id="userName"
+                            name="userName"
+                        />
+                    </div>
+                    <div className="col-sm-6 form-group">
+                        <label htmlFor="displayName">Display Name</label>
+                        <input
+                            required
+                            value={user.displayName}
+                            onChange={e => setUser({ ...user, displayName: e.target.value })}
+                            className="form-control"
+                            id="displayName"
+                            name="displayName"
+                        />
+                    </div>
+                    <div className="col-sm-6 form-group">
 						<label htmlFor="password">Password</label>
 						<input
 							type="password"
 							required
 							value={user.password}
-							onChange={e => setUser({ ...user, password: e.target.value })}
+							onChange={e => setUser({ ...user, password: e.target.value})}
 							className="form-control"
 							id="password"
 							name="password"
-						/>
-					</div>
-				</div>
-				<input className="btn btn-primary" type="submit" value="Sign Up" />
-			</form>
-		</main>
-	)
+							/>
+                    </div>
+                </div>
+                <input className="btn btn-primary" type="submit" value="Sign Up" />
+            </form>
+        </main>
+    )
 }
 
 export default SignUpForm
